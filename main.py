0cc18b1e94f103041344e4d02fa0240e9c666d90
Emil Seredin <emils.seredins@gmail.com>
2025-05-01 18:39:27 +0300

    refactor video, audio and playlist download

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pytube import Playlist, YouTube
+from pytubefix import Playlist, YouTube
 import os
 import argparse
 
@@ -8,7 +8,7 @@ AUDIO_DIR = f'{os.path.expanduser("~")}/Music'
 
 parser = argparse.ArgumentParser(description="Download videos/playlists from YouTube")
 parser.add_argument("--link", type=str, help="link of a playlist/video")
-parser.add_argument("--start_with", type=int, 
+parser.add_argument("--start", type=int, 
     help="position of a video in the playlist to start downloading from")
 parser.add_argument("--nth_video", type=int, 
     help="download single video from a playlist")
@@ -23,71 +23,39 @@ def create_dir(dir_path: str) -> bool:
         os.mkdir(dir_path)
 
 
-def download_playlist(link: str):
+def download_playlist(link: str, start: int):
     playlist = Playlist(link)
+    videos = enumerate(playlist.videos, start=start)
     print(f'Playlist: {playlist.title}')
     playlist_title = "-".join(playlist.title.split("/"))
-    current_playlist_path = f'{VIDEO_DIR}/{playlist_title}'
-    create_dir(current_playlist_path)
-    video_counter = 0
-    start_from = 0
-    v_num = False
-    if args.start_with:
-        start_from = args.start_with
-    if args.nth_video:
-        v_num = args.nth_video
-    for video in playlist.videos:
-        video_counter += 1
-        if video_counter < start_from:
-            continue
-        if v_num and video_counter < v_num:
-            continue
-        if v_num and video_counter > v_num:
-            break
-        try:
-            title = video.title
-        except Exception:
-            print("Not able to access video title.")
-            title = ""
-        else:
-            if "/" in title:
-                title = "-".join(title.split("/"))
-        title = f"{video_counter}_{video.title}"    
+    playlist_path = f'{VIDEO_DIR}/{playlist_title}'
+    create_dir(playlist_path)
+    for index, video in videos:
+        title = video.title
+        if "/" in title:
+            title = "-".join(title.split("/"))
+        title = f"{index + 1}_{title}.mp4"  
+          
         print(f'Downloading video: {title}')
-        video.streams.get_highest_resolution().download(output_path=current_playlist_path,filename=title)
+        video.streams.get_highest_resolution().download(
+            output_path=playlist_path,
+            filename=title)
 
 
 def download_video(link: str):
     video = YouTube(link)
     print(f'Downloading video: {video.title}')
-    if "/" in video.title:
-        video.title = "-".join(video.title.split("/"))
-    # print(video.streams)
-    # itag = int(input("Enter the stream itag: "))
-    # print(itag)
-    # itag = 271
-    # video_stream = video.streams.get_by_itag(itag)
-    video_stream = None
-    if video_stream:
-        video_stream.download(output_path=VIDEO_DIR, filename=video.title)
-    else:
-        # print("Something went wrong. Downloading lower resolution video.")
-        video.streams.get_highest_resolution().download(output_path=VIDEO_DIR, filename=video.title)
+    title = video.title
+    if "/" in title:
+        title = "-".join(title.split("/"))
+    video.streams.get_highest_resolution().download(output_path=VIDEO_DIR, filename=title)
 
 
 def download_audio(link: str):
-    try:
-        video = YouTube(link)
-        video.streams.get_audio_only().download(output_path=AUDIO_DIR, filename=video.title)
-    except KeyError:
-        print('KeyError')
-        errors += 1
-        if errors == 10:
-            return
-        else:
-            download_audio(link)
-    except Exception as err:
-        print(f"General error!\n{err}")
+    video = YouTube(link)
+    video.streams.get_audio_only().download(
+        output_path=AUDIO_DIR,
+        filename=video.title)
 
         
 def download(link: str):
@@ -96,10 +64,10 @@ def download(link: str):
     elif "watch" in link:
         download_video(link=link)
     elif "playlist" in link:
-        download_playlist(link=link)
+        start = args.start - 1 if args.start else 0
+        download_playlist(link=link, start=start)
     else:
         print("Please provide a valid youtube link")
-        return
 
 
 def main():
